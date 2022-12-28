cbd7090235625ee7d50d4d2949684d6c0fe2bc81
SnowflakeGreg <121560896+SnowflakeGreg@users.noreply.github.com>
2022-12-28 15:45:29 +0100

    Update streamlit_app.py
    Add texts 2 and 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit
 streamlit.title('My Partner New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 2 & Brueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
