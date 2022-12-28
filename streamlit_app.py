eca0918a9d2af4c4f44a00b228a8261daccb23d9
SnowflakeGreg <121560896+SnowflakeGreg@users.noreply.github.com>
2022-12-28 15:49:00 +0100

    Update streamlit_app.py
    Add Icons to the text and new text "Avocado Toast"

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title('My Partner New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 2 & Brueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣Omega 2 & Brueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
 
