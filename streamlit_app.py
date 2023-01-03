cd275bb32b5f2c9a28d0a6d3b8fab13e75c7f256
SnowflakeGreg <121560896+SnowflakeGreg@users.noreply.github.com>
2023-01-03 20:59:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 #stop here
-stramlit.stop()
+streamlit.stop()
 
 #snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
